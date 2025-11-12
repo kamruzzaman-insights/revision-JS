d0834f5ebf333251f1f2e0994e4101d70278c085
kamruzzaman-insights <kamruzzaman.insights@gmail.com>
2025-11-13 01:20:27 +0600

    basic js

---
+++ variable.js
@@ -0,0 +1,11 @@
+const friends = ["shamim", "umor", "limon", "shiab"];
+
+// friends.push("oalide");
+// friends.pop();
+// friends.unshift("raihan");
+// friends.shift();
+// console.log(friends.slice(1, 3));
+
+const item = friends.splice(0, 1);
+console.log(item);
+
